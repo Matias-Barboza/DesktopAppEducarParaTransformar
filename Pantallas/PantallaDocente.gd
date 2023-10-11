6216780a2317c245870e9e0fe463fa38a06f45e1
Matias Barboza <matiasbarboza540@gmail.com>
2023-10-11 16:04:49 -0300

    Para vos campita

--- Pantallas/PantallaDocente.gd
+++ Pantallas/PantallaDocente.gd
@@ -1,100 +1,105 @@
 extends Control
 
+var division = 1
+var desplegado = false
+var paneles = []
+var headers = ["Content-Type: application/json"]
+var endpoint = Globals.URL + "/api/teachers/classes/" + str(Globals.userId)
+var endpointAlumnos = Globals.URL + "/api/divisions/" + str(division) + "/students"
+var listaMaterias = {}
+var listaDivisiones = {}
+
+
 
 onready var request = $HTTPRequest
-onready var conjuntoAlumnos = $PanelDatos/Alumnos
-onready var labelMateria = $Panel/LabelNombreMateria
-
-
-export var alum: PackedScene
-
-var alumnos = 0
-var listaLegajoAlumnos = {}
-var listaNombreAlumnos = {}
-var listaApellidoAlumnos = {}
-var listaNotaAlumnos = {}
-var endpoint = Globals.URL + "/api/students"
-
-var students
-
+onready var requestAlumnos = $GetAlumnos
+onready var animation_player = $AnimationPlayer
+onready var panel_horario = $PanelHorario
+onready var panel_bienvenida = $PanelBienvenida
+onready var panel_notas = $PanelNotas
+onready var panel_materias = $PanelMaterias
+onready var menu_materias = $PanelMaterias/Panel/OptionButton
+onready var panel_materias_especificas = $PanelMateriaEspecifica
+onready var label_nombre_materias = $PanelMateriaEspecifica/NombreMateria
+onready var label_bienvenida = $PanelBienvenida/LabelBienvenida
 
 func _ready():
 	request.request(endpoint)
+	panel_bienvenida.visible = true
+	panel_horario.visible = false
+	panel_notas.visible = false
+	panel_materias.visible = false
+	panel_materias_especificas.visible = false
+	label_bienvenida.text = "Bienvenido " + Globals.nombreCompleto
 	
-
+	paneles = [panel_bienvenida, panel_horario, panel_notas, panel_materias, panel_materias_especificas]
+	
 
 func _on_HTTPRequest_request_completed(result, response_code, headers, body):
 	if response_code == 200:
-		
-		var json = JSON.parse(body.get_string_from_utf8())
-		students = json
-		
 		var i = 0
-		for alumno in json.result:
-			listaLegajoAlumnos[i] = alumno.file_number
-			listaNombreAlumnos[i] = alumno.firstname
-			listaApellidoAlumnos[i] = alumno.lastname
-			#listaNotaAlumnos[i] = alumno.id
-			alumnos += 1
-			i+= 1
-		
-		instanciarAlumnos()
-		espaciarAlumnos()
-		darValores()
+		var json = JSON.parse(body.get_string_from_utf8())
+		for materia in json.result:
+			listaMaterias[i] = materia["class_name"] + " ("+ materia.division.division_name + ")"
+			listaDivisiones[i] = materia.division.id
+			menu_materias.get_popup().add_item(materia["class_name"] + " ("+ materia.division.division_name + ")", i)
+			i += 1
+	else:
+		print(body)
+
+
+func _on_GetAlumnos_request_completed(result, response_code, headers, body):
+	if response_code == 200:
+		var json = JSON.parse(body.get_string_from_utf8())
+		print("Mostrando alumnos de la division " + str(division))
 	else:
 		print("error")
 
 
-func instanciarAlumnos():
-	var x = alumnos
-	while x > 0:
-		conjuntoAlumnos.add_child(alum.instance())
-		x -= 1
 
-
-func espaciarAlumnos():
-	var posicionInicial = Vector2.ZERO
-	var i = 1
-	while i < alumnos:
-		var hijo = conjuntoAlumnos.get_child(i)
-		posicionInicial = conjuntoAlumnos.get_child(i-1).get_position()
-		hijo.rect_position.y = posicionInicial.y + 35 
-		i += 1
-
-
-func darValores():
-	var i = 0
-	while i < alumnos:
-		var hijo = conjuntoAlumnos.get_child(i)
-		hijo.get_node("LabelLegajo").text = listaLegajoAlumnos[i]
-		hijo.get_node("LabelNombre").text = listaNombreAlumnos[i]
-		hijo.get_node("LabelApellido").text = listaApellidoAlumnos[i]
-		#hijo.get_node("LabelNota").text = "%s" % listaNotaAlumnos[i]
-		i += 1
-
-
-func _on_Imprimir_PDF_request_completed(result, response_code, headers, body):
-	if response_code == 201:
-		var json = JSON.parse(body.get_string_from_utf8())
-		OS.shell_open(json.result.response)
+func _on_ButtonMenuDesplegable_pressed():
+	if not desplegado:
+		animation_player.play("despliegue_menu_lateral")
 	else:
-		print("Error en la solicitud. Código de respuesta:", response_code)
+		animation_player.play("repliegue_menu_lateral")
+	desplegado = not desplegado
 
 
-func _on_Button_pressed():
-	var header = [ "content-type: application/json" , "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhMThiZWVhMjdmZDgxMzA5ZWFhZTA5NGFkMmU1NjQyOTU4NmMwNzBmMGY5MDdmYzM1ZTI0NWI3NjEwYTY4ODgzIiwic3ViIjoiYWxlam9vY3p0d2l0Y2hAZ21haWwuY29tIiwiZXhwIjo5OTk5OTk5OTk5fQ._-fbaWRNvua_SmCWQ48U2apGTdc_2_PW-Nga9IG1qxQ" ]
-	var URL = "https://us1.pdfgeneratorapi.com/api/v4/documents/generate"
+func activar_panel(panel_a_visibilizar):
+	for panel in paneles:
+		if panel != panel_a_visibilizar:
+			panel.visible = false
+		else:
+			panel.visible = true
+	animation_player.play("repliegue_menu_lateral")
+	desplegado = not desplegado
+
+
+func _on_ButtonHorarios_pressed():
+	activar_panel(panel_horario)
+
+func _on_ButtonNotas_pressed():
+	activar_panel(panel_notas)
+
+
+func _on_ButtonMaterias_pressed():
+	activar_panel(panel_materias)
 	
-	var json_data = {
-		"template": {
-			"id": 808188,
-			"data": {
-				"students": students.result
-			}
-		},
-		"format": "pdf",
-		"output": "url",
-		"name": "Listado Alumnos"
-	}
-	print(JSON.print(json_data))
-	$"Imprimir PDF".request(URL, header, true, HTTPClient.METHOD_POST, JSON.print(json_data))
+
+func _on_OptionButton_item_selected(index):
+	label_nombre_materias.text = listaMaterias[index]
+	division = listaDivisiones[index]
+	requestAlumnos.request(endpointAlumnos)
+	activar_panel(panel_materias_especificas)
+	menu_materias.selected = -1
+	
+
+
+func _on_ButtonSalir_pressed():
+	Globals.userId = 0
+	Globals.jwt = ""
+	Globals.password = ""
+	Globals.nombreCompleto = ""
+	get_tree().change_scene("res://Pantallas/PantallaInicioDeSesion.tscn")
+
+
---
+++ Pantallas/ScriptPDF.gd
@@ -0,0 +1,100 @@
+extends Control
+
+
+onready var request = $HTTPRequest
+onready var conjuntoAlumnos = $PanelDatos/Alumnos
+onready var labelMateria = $Panel/LabelNombreMateria
+
+
+export var alum: PackedScene
+
+var alumnos = 0
+var listaLegajoAlumnos = {}
+var listaNombreAlumnos = {}
+var listaApellidoAlumnos = {}
+var listaNotaAlumnos = {}
+var endpoint = Globals.URL + "/api/students"
+
+var students
+
+
+func _ready():
+	request.request(endpoint)
+	
+
+
+func _on_HTTPRequest_request_completed(result, response_code, headers, body):
+	if response_code == 200:
+		
+		var json = JSON.parse(body.get_string_from_utf8())
+		students = json
+		
+		var i = 0
+		for alumno in json.result:
+			listaLegajoAlumnos[i] = alumno.file_number
+			listaNombreAlumnos[i] = alumno.firstname
+			listaApellidoAlumnos[i] = alumno.lastname
+			#listaNotaAlumnos[i] = alumno.id
+			alumnos += 1
+			i+= 1
+		
+		instanciarAlumnos()
+		espaciarAlumnos()
+		darValores()
+	else:
+		print("error")
+
+
+func instanciarAlumnos():
+	var x = alumnos
+	while x > 0:
+		conjuntoAlumnos.add_child(alum.instance())
+		x -= 1
+
+
+func espaciarAlumnos():
+	var posicionInicial = Vector2.ZERO
+	var i = 1
+	while i < alumnos:
+		var hijo = conjuntoAlumnos.get_child(i)
+		posicionInicial = conjuntoAlumnos.get_child(i-1).get_position()
+		hijo.rect_position.y = posicionInicial.y + 35 
+		i += 1
+
+
+func darValores():
+	var i = 0
+	while i < alumnos:
+		var hijo = conjuntoAlumnos.get_child(i)
+		hijo.get_node("LabelLegajo").text = listaLegajoAlumnos[i]
+		hijo.get_node("LabelNombre").text = listaNombreAlumnos[i]
+		hijo.get_node("LabelApellido").text = listaApellidoAlumnos[i]
+		#hijo.get_node("LabelNota").text = "%s" % listaNotaAlumnos[i]
+		i += 1
+
+
+func _on_Imprimir_PDF_request_completed(result, response_code, headers, body):
+	if response_code == 201:
+		var json = JSON.parse(body.get_string_from_utf8())
+		OS.shell_open(json.result.response)
+	else:
+		print("Error en la solicitud. Código de respuesta:", response_code)
+
+
+func _on_Button_pressed():
+	var header = [ "content-type: application/json" , "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhMThiZWVhMjdmZDgxMzA5ZWFhZTA5NGFkMmU1NjQyOTU4NmMwNzBmMGY5MDdmYzM1ZTI0NWI3NjEwYTY4ODgzIiwic3ViIjoiYWxlam9vY3p0d2l0Y2hAZ21haWwuY29tIiwiZXhwIjo5OTk5OTk5OTk5fQ._-fbaWRNvua_SmCWQ48U2apGTdc_2_PW-Nga9IG1qxQ" ]
+	var URL = "https://us1.pdfgeneratorapi.com/api/v4/documents/generate"
+	
+	var json_data = {
+		"template": {
+			"id": 808188,
+			"data": {
+				"students": students.result
+			}
+		},
+		"format": "pdf",
+		"output": "url",
+		"name": "Listado Alumnos"
+	}
+	print(JSON.print(json_data))
+	$"Imprimir PDF".request(URL, header, true, HTTPClient.METHOD_POST, JSON.print(json_data))
